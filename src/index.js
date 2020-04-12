d5116dbd7407cbdf859e4f15939da09d99a157cc
Vitaliy Solomka <dkvvs@ukr.net>
2020-04-12 22:30:58 +0300

    add homework-11-promisification

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import './styles.css';
+
+import delayExec from './js/task-01';
+import usersActiv from './js/task-02';
+import transactionId from './js/task-03';
+
+delayExec();
+usersActiv();
+transactionId();
